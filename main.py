98acecf6c17d01dcc68e82995ad199d5f5098cf6
Tyler Pawlaczyk <tyler.pawlaczyk@millennialfintech.com>
2019-10-01 11:10:02 -0400

    Started file parsing ability

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import argparse
 from circuit import Lightbulb, Switch
 
 
+def generate_switches(num):
+    """Generate num switches"""
+    switches = []
+    for i in range(0, num):
+        switches.append(Switch())
+    return switches
+
 def parse_file(path):
     num_lightbulbs = 0
     num_switches = 0
     with open(path, 'r') as in_file:
-        # get number of lightbulbs
-        line = in_file.readline()
-        num_lightbulbs = int(line)
-        
         # get number of switches
         line = in_file.readline()
         num_switches = int(line)
-    
+        # create switches
+        switches = generate_switches(num_switches)
 
-
-
-
+        # get number of lightbulbs
+        line = in_file.readline()
+        num_lightbulbs = int(line)
+        # generate lightbulbs
 
 def main():
 
@@ -38,6 +43,7 @@ def main():
     print("s2: " + str(s2.state()))
     print("l1: " + str(l1.state()))
 
+    print(len(generate_switches(100)))
 
 if __name__ == "__main__":
     main()
